98721febdf85add1bd33466f5ec4e320d2137775
JiaxingYuan <53160887+JiaxingYuan@users.noreply.github.com>
2021-01-29 18:45:42 +0800

    Update fix_charge_regulation.cpp

--- fix_charge_regulation.cpp
+++ fix_charge_regulation.cpp
@@ -13,7 +13,7 @@
 ------------------------------------------------------------------------- */
 
 /* ----------------------------------------------------------------------
-   Contributing author: Tine Curk and Jiaxing Yuan
+   Contributing author: Tine Curk (curk@northwestern.edu) and Jiaxing Yuan
 ------------------------------------------------------------------------- */
 #include "fix_charge_regulation.h"
 #include <mpi.h>
